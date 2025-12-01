9c90b4ff21e237085ea04f294587c025f663db4e
Rinn <ariffrafie2009@gmail.com>
2025-12-02 02:41:47 +0800

    Fix SQL delete statement formatting

--- pengundi_delete.php
+++ pengundi_delete.php
@@ -3,7 +3,7 @@
 
   $idpengundi = $_GET["idpengundi"];
 
-  $sql = "delete from pengundi where idpengundi = '$idpengundi'";
+  $sql = "delete from pengundi where idpengundi = '$idpengundi' ";
   $result = mysqli_query($sambungan, $sql);
 
   echo "<script>window.location='pengundi_senarai.php'</script>";
