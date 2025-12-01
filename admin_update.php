9f329be0b8a0dde9c5345b040efd14de4e367db6
Rinn <ariffrafie2009@gmail.com>
2025-12-02 00:00:09 +0800

    Refactor admin_update.php for better readability

--- admin_update.php
+++ admin_update.php
@@ -13,21 +13,21 @@ include("admin_menu.php");
 
     $sql = "update admin
             set namaadmin = '$namaadmin', password = '$password'
-            where idadmin = '$idadmin'";
+            where idadmin = '$idadmin' ";
     $result = mysqli_query($sambungan, $sql);
-    if ($result == TRUE) 
+    if ($result == true) 
         echo "<h4>Berjaya kemaskini</h4>";
     else
-        echo "<h4>Ralat : $sql<br>".mysqli_error( $sambungan)."</h4>";
+        echo "<h4>Ralat : $sql<br>".mysqli_error($sambungan)."</h4>";
 }
 
-//idadmin dari fail admin_senarai.php
- if(isset($_GET['idadmin']))
-    $idadmin = $_GET['idamin'];
+// idadmin dari fail admin_senarai.php
+ if (isset($_GET['idadmin']))
+    $idadmin = $_GET['idadmin'];
 
- $sql = "select * from admin where idadmin = '$idadmin'";
+ $sql = "select * from admin where idadmin = '$idadmin' ";
  $result = mysqli_query($sambungan, $sql);
- while ($admin = mysqli_fetch_array($result)) {
+ while($admin = mysqli_fetch_array($result)) {
     $password = $admin['password'];
     $namaadmin = $admin['namaadmin'];
  }
@@ -41,15 +41,15 @@ include("admin_menu.php");
     <table>
         <tr>
             <td>ID Admin</td>
-            <td><input type="text" name="idadmin" value="<?php echo $idadmin; ?>"></td>
+            <td><input type="text" name="idadmin" value="<?php echo $idadmin; ?>" ></td>
         </tr>
         <tr>
             <td>Nama Admin</td>
-            <td><input type="text" name="namaadmin" value="<?php echo $namaadmin; ?>"></td>
+            <td><input type="text" name="namaadmin" value="<?php echo $namaadmin; ?>" ></td>
         </tr>
         <tr>
             <td>Password</td>
-            <td><input type="text" name="password" value="<?php echo $password; ?>"></td>
+            <td><input type="text" name="password" value="<?php echo $password; ?>" ></td>
         </tr>
     </table>
     <button class="update" type="submit" name="submit">Update</button>
