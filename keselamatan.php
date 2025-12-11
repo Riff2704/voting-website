f3d3919794761437a7746eea86fc63b6c16d57e6
Rinn <ariffrafie2009@gmail.com>
2025-12-12 01:11:27 +0800

    Update keselamatan.php

--- keselamatan.php
+++ keselamatan.php
@@ -2,7 +2,7 @@
   if (session_status() == PHP_SESSION_NONE) {
      session_start();
   }
-  if (!isset($_SESSION["idpengguna"])) {
-      echo "<script>window.location='logout.php'</script>";
-  }
+  //if (!isset($_SESSION["idpengguna"])) {
+  //    echo "<script>window.location='logout.php'</script>";
+  //}
 ?>
